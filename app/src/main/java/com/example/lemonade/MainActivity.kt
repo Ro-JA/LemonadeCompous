74970a78bb98975e52c6157593c2fbee72273bd0
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-03-11 22:04:10 +0900

    work

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.example.lemonade
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -20,7 +22,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
-import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,23 +42,55 @@ fun lemonedApp() {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
+        TextAndImageLemoned(
+            textLabelResourceId = R.string.tree,
+            drawableResourceId = R.drawable.lemon_tree,
+            contentDescriptionResourceId = R.string.lemon_tree,
+            onImageClick = { /*TODO*/ })
+
     }
 }
 
 //функция для интерфейса
 @Composable
-fun textAndImageLemoned(
+fun TextAndImageLemoned(
 //    параметр для текста над картинкой
     textLabelResourceId: Int,
 //    параметр для изображения лемона
     drawableResourceId: Int,
 //    параметр для описания изоброжения
     contentDescriptionResourceId: Int,
-//
+// метод для нажатия на картинку
     onImageClick: () -> Unit,
-
-
+// параметр для модификатора
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
     ) {
+        Text(
+            text = stringResource(textLabelResourceId), fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.padding(16.dp))
+        Image(
+            painter = painterResource(drawableResourceId),
+            contentDescription = stringResource(contentDescriptionResourceId),
+            modifier = Modifier
+                .wrapContentSize()
+                .clickable {
+                    onImageClick
+                }
+                .border(
+                    BorderStroke(2.dp, Color(105, 205, 216)),
+                    shape = RoundedCornerShape(4.dp)
+                )
+                .padding(16.dp)
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
